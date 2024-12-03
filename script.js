bd7645f6f000443ffeb7d0e580db3defa9cc0978
sarveshpadhiyar <sarveshpadhiyar2@gmail.com>
2024-12-02 21:47:13 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Example JavaScript functionality (smooth scroll)
+document.querySelectorAll('a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
